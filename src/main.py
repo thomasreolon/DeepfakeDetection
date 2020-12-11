461bc3136a341094bec024eff017c73215978beb
thomasreolon <thomas.reolon.it@gmail.com>
2020-12-11 22:59:12 +0100

    add example of library usage in main.py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
-from openface.OpenFaceAPI import OpenFaceAPI
+from openface import OpenFaceAPI
 
-PATH_TO_OPENFACE_DIR
+PATH_TO_OPENFACE_DIR = '/home/tom/Desktop/OpenFace'
 
 
+
+# setup API to call OpenFace functions
 openfaceAPI = OpenFaceAPI(PATH_TO_OPENFACE_DIR)
 
 
-
-
+# process an image
+openfaceAPI.get_faceLand('../test_data/img/lena.png')
