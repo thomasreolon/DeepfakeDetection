60de11aa058bba99ed1a790ba27330f770561d1d
Moreno98 <morenodinca@gmail.com>
2020-12-21 22:16:38 +0100

    Fix some installation's bugs, now it works

--- src/openface/installer.py
+++ src/openface/installer.py
@@ -21,20 +21,21 @@ def install_openface():
         os.chdir(mypath+'/OpenFace')
         os.system('bash ./download_models.sh')
 
-        # changing some lines of install.sh (allow opencv to decode videos)
-        with open('./install.sh', 'r') as f:
+        # # changing some lines of install.sh (allow opencv to decode videos)
+        with open(f'{mypath}/OpenFace/install.sh', 'r') as f:
             text = f.read()
 
-        change1 = "sudo apt-get -y install libtbb2 libjpeg-dev libpng-dev libtiff-dev libdc1394-22-dev\nsudo apt-get install ffmpeg\nsudo apt-get install libavcodec-dev libavformat-dev libavdevice-dev"
+        change1 = "sudo apt-get -y install libtbb2 libjpeg-dev libpng-dev libtiff-dev libdc1394-22-dev\nsudo apt-get -y install ffmpeg\nsudo apt-get -y install libavcodec-dev libavformat-dev libavdevice-dev"
         text = re.sub('sudo apt-get -y install libtbb2 libjpeg-dev libpng-dev libtiff-dev libdc1394-22-dev', change1, text)
 
-        change2 = "cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local  -D WITH_FFMPEG=ON -D #WITH_TBB=ON -D WITH_CUDA=OFF -D BUILD_SHARED_LIBS=OFF WITH_GSTREAMER=ON .."
+        change2 = "cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_FFMPEG=ON -D WITH_TBB=ON -D WITH_CUDA=OFF -D BUILD_SHARED_LIBS=OFF WITH_GSTREAMER=ON .."
         text = re.sub( "cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D WITH_CUDA=OFF -D BUILD_SHARED_LIBS=OFF ..", change2, text)
 
-        with open('./install.sh', 'w') as f:
+        with open(f'{mypath}/OpenFace/install.sh', 'w') as f:
             f.write(text)
 
         # install OpenFace
+        os.chdir(mypath+'/OpenFace')
         os.system(f'echo {sudo_passw} | sudo bash ./install.sh ')
         os.chdir(oldpath)
     else:
