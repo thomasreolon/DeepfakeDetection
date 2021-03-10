c2702fb827d40bf57a3d5f6a718ca650246412b4
thomasreolon <thomas.reolon.it@gmail.com>
2021-03-10 19:56:14 +0100

    suming  automatic plots

--- src/test_landmarks.py
+++ src/test_landmarks.py
@@ -3,7 +3,13 @@ from videoanalizer import VideoAnalizer
 
 vd = VideoAnalizer()
 
-clf = vd.train_OneClassSVM('../test_data/videos/real/thomas1')
 
+# Video with Landmarks
+
+clf = vd.train_OneClassSVM('../test_data/videos/real/thomas1')
 print('startss')
 clf.predict_video('../test_data/videos/fake/thomas1/fake_me.mp4', landmark_video=True)
+
+
+# Why are classifiers misbehaving?
+vd.plot_features([['../test_data/videos/real/Obama'], ['../test_data/videos/fake/Obama']], labels=['real', 'fake']) 
