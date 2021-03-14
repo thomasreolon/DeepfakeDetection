7cc4359d87a30b12173969c8e3c8ca8703b229ab
thomasreolon <thomas.reolon.it@gmail.com>
2021-03-14 13:29:41 +0100

    added a reference

--- src/gdrive/__init__.py
+++ src/gdrive/__init__.py
@@ -1,1 +1,7 @@
 from .google_drive_downloader import GoogleDriveDownloader
+
+# Code implemented by Andrea Palazzi: https://github.com/ndrplz/google-drive-downloader
+
+# using the answer from
+# https://stackoverflow.com/questions/38511444/python-download-files-from-google-drive-using-url
+
